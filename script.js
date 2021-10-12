92d4184acd338a1c2385bf2707e14062fffba9f1
Chris Soncrant <christopher.soncrant@gmail.com>
2021-10-12 12:09:21 -0700

    added styling for custom button and programmed its script

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ const totalDisplay = document.getElementById("total-display");
 const tipDisplay = document.getElementById("tip-display");
 const buttons = document.querySelectorAll("button");
 
+const customButtonLabel = document.getElementById("custom");
+const customButtonContainer = document.querySelector(".custom-container");
+const customInput = document.getElementById("custom-input");
+const customError = document.querySelector(".custom-error");
+const customSubmit = document.getElementById("custom-submit");
+
+
+
 
 //Test Strings
 let testInput1 = "jkahdf.lkadhf";
@@ -35,6 +43,8 @@ function removeErrorMessage() {
     errorMessage[1].style.display = "none";
     billInput.classList.remove('error');
     numOfPeople.classList.remove('error');
+    customError.style.display = "none";
+    customInput.style.height = "100%";
 }
 
 
@@ -56,24 +66,26 @@ function clickButton() {
                 return changeOutlineColor(billInput);
             } else {
                 removeErrorMessage();
-            }
+            } 
 
             if ((/[^0-9.]/).test(billInput.value)) {
                 errorMessage[0].style.display = "block";
                 errorMessage[0].textContent = "Invalid characters!"
                 return changeOutlineColor(billInput);
+            } else {
+                removeErrorMessage();
             }
 
             // Checking Number of People Input Value:
             if (numOfPeople.value === "" || numOfPeople.value == 0) {
                 errorMessage[1].style.display = "block";
-                errorMessage[1].textContent = "Enter a nonzero value!"
+                errorMessage[1].textContent = "Enter nonzero value!"
                 return changeOutlineColor(numOfPeople);
             }
 
             if (!(countDecimals(numOfPeople.value) === 0)) {
                 errorMessage[1].style.display = "block";
-                errorMessage[1].textContent = "Too many decimals!"
+                errorMessage[1].textContent = "No decimals!"
                 return changeOutlineColor(numOfPeople);
             }
 
@@ -87,72 +99,90 @@ function clickButton() {
 
             if (button.textContent === "5%") {
                 let tipAmount = .05 * billInput.value;
-                let totalAmount = Number(billInput.value) + tipAmount;
-                if (numOfPeople.value === "" || numOfPeople.value == 0 || numOfPeople.value == 1) {
-                    tipDisplay.textContent = "$" + tipAmount.toFixed(2);
-                    totalDisplay.textContent = "$" + totalAmount.toFixed(2);
-                } else {
-                    tipAmount = (tipAmount/numOfPeople.value)
-                    totalAmount = billInput.value/numOfPeople.value + tipAmount;               
-                    tipDisplay.textContent = "$" + tipAmount.toFixed(2);
-                    totalDisplay.textContent = "$" + totalAmount.toFixed(2);
-                }
+                let totalAmount = Number(billInput.value) + tipAmount;                
+                tipAmount = (tipAmount/numOfPeople.value)
+                totalAmount = billInput.value/numOfPeople.value + tipAmount;               
+                tipDisplay.textContent = "$" + tipAmount.toFixed(2);
+                totalDisplay.textContent = "$" + totalAmount.toFixed(2);
             }
 
             if (button.textContent === "10%") {
                 let tipAmount = .10 * billInput.value;
-                let totalAmount = Number(billInput.value) + tipAmount;
-                if (numOfPeople.value === "" || numOfPeople.value == 0 || numOfPeople.value == 1) {
-                    tipDisplay.textContent = "$" + tipAmount.toFixed(2);
-                    totalDisplay.textContent = "$" + totalAmount.toFixed(2);
-                } else {
-                    tipAmount = (tipAmount/numOfPeople.value)
-                    totalAmount = billInput.value/numOfPeople.value + tipAmount;               
-                    tipDisplay.textContent = "$" + tipAmount.toFixed(2);
-                    totalDisplay.textContent = "$" + totalAmount.toFixed(2);
-                }
+                let totalAmount = Number(billInput.value) + tipAmount;               
+                tipAmount = (tipAmount/numOfPeople.value)
+                totalAmount = billInput.value/numOfPeople.value + tipAmount;               
+                tipDisplay.textContent = "$" + tipAmount.toFixed(2);
+                totalDisplay.textContent = "$" + totalAmount.toFixed(2);
+                
             }
 
             if (button.textContent === "15%") {
                 let tipAmount = .15 * billInput.value;
-                let totalAmount = Number(billInput.value) + tipAmount;
-                if (numOfPeople.value === "" || numOfPeople.value == 0 || numOfPeople.value == 1) {
-                    tipDisplay.textContent = "$" + tipAmount.toFixed(2);
-                    totalDisplay.textContent = "$" + totalAmount.toFixed(2);
-                } else {
-                    tipAmount = (tipAmount/numOfPeople.value)
-                    totalAmount = billInput.value/numOfPeople.value + tipAmount;               
-                    tipDisplay.textContent = "$" + tipAmount.toFixed(2);
-                    totalDisplay.textContent = "$" + totalAmount.toFixed(2);
-                }
+                let totalAmount = Number(billInput.value) + tipAmount;                
+                tipAmount = (tipAmount/numOfPeople.value)
+                totalAmount = billInput.value/numOfPeople.value + tipAmount;               
+                tipDisplay.textContent = "$" + tipAmount.toFixed(2);
+                totalDisplay.textContent = "$" + totalAmount.toFixed(2);               
             }
 
             if (button.textContent === "25%") {
                 let tipAmount = .25 * billInput.value;
                 let totalAmount = Number(billInput.value) + tipAmount;
-                if (numOfPeople.value === "" || numOfPeople.value == 0 || numOfPeople.value == 1) {
-                    tipDisplay.textContent = "$" + tipAmount.toFixed(2);
-                    totalDisplay.textContent = "$" + totalAmount.toFixed(2);
-                } else {
-                    tipAmount = (tipAmount/numOfPeople.value)
-                    totalAmount = billInput.value/numOfPeople.value + tipAmount;               
-                    tipDisplay.textContent = "$" + tipAmount.toFixed(2);
-                    totalDisplay.textContent = "$" + totalAmount.toFixed(2);
-                }
+                tipAmount = (tipAmount/numOfPeople.value)
+                totalAmount = billInput.value/numOfPeople.value + tipAmount;               
+                tipDisplay.textContent = "$" + tipAmount.toFixed(2);
+                totalDisplay.textContent = "$" + totalAmount.toFixed(2);    
             }
 
             if (button.textContent === "50%") {
                 let tipAmount = .50 * billInput.value;
                 let totalAmount = Number(billInput.value) + tipAmount;
-                if (numOfPeople.value === "" || numOfPeople.value == 0 || numOfPeople.value == 1) {
-                    tipDisplay.textContent = "$" + tipAmount.toFixed(2);
-                    totalDisplay.textContent = "$" + totalAmount.toFixed(2);
+                tipAmount = (tipAmount/numOfPeople.value)
+                totalAmount = billInput.value/numOfPeople.value + tipAmount;               
+                tipDisplay.textContent = "$" + tipAmount.toFixed(2);
+                totalDisplay.textContent = "$" + totalAmount.toFixed(2);
+                
+            }
+
+            if (button.textContent === "Custom") {
+                customButtonLabel.style.display = "none";
+                customButtonContainer.style.display = "flex";
+            }
+
+            if (button.textContent === ">") {
+                if (customInput.value === "") {
+                    customError.style.display = "block";
+                    customError.textContent = "Enter a value!"
+                    customInput.style.height = "75%";
+                    return changeOutlineColor(customInput);
                 } else {
-                    tipAmount = (tipAmount/numOfPeople.value)
-                    totalAmount = billInput.value/numOfPeople.value + tipAmount;               
-                    tipDisplay.textContent = "$" + tipAmount.toFixed(2);
-                    totalDisplay.textContent = "$" + totalAmount.toFixed(2);
+                    removeErrorMessage();
                 }
+                
+                if (countDecimals(customInput.value) === 2) {
+                    customError.style.display = "block";
+                    customError.textContent = "Too many decimals!";
+                    customInput.style.height = "75%";
+                    return changeOutlineColor(customInput);
+                } else {
+                    removeErrorMessage();
+                } 
+    
+                if ((/[^0-9.]/).test(customInput.value)) {
+                    customError.style.display = "block";
+                    customError.textContent = "Invalid entry!"
+                    customInput.style.height = "75%";
+                    return changeOutlineColor(customInput);
+                } else {
+                    removeErrorMessage();customInput
+                }
+                
+                let tipAmount = (customInput.value/100) * billInput.value;
+                let totalAmount = Number(billInput.value) + tipAmount;
+                tipAmount = (tipAmount/numOfPeople.value)
+                totalAmount = billInput.value/numOfPeople.value + tipAmount;               
+                tipDisplay.textContent = "$" + tipAmount.toFixed(2);
+                totalDisplay.textContent = "$" + totalAmount.toFixed(2);
             }
 
             if (button.textContent ==="RESET") {
